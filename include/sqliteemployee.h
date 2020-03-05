cfbd8cd2bdc6e473640c37f1f9ba3acb51d5f3af
Hanquet Brian <he201459@students.ephec.be>
2020-03-05 15:52:05 +0100

    premier test sur sqlite

--- include/sqliteemployee.h
+++ include/sqliteemployee.h
@@ -9,9 +9,20 @@ public:
     sqliteEmployee();
 
     void connect();
+    void close();
 
 private:
-    sqlite3 *db;
+    sqlite3 *m_db;
+
+    void createDefaultDataBase();
+    int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+        int i;
+        for(i = 0; i<argc; i++) {
+           printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        }
+        printf("\n");
+        return 0;
+    }
 };
 
 #endif // SQLITEEMPLOYEE_H
--- src/sqliteemployee.cpp
+++ src/sqliteemployee.cpp
@@ -8,11 +8,28 @@ sqliteEmployee::sqliteEmployee()
 }
 
 void sqliteEmployee::connect() {
-    int rc = sqlite3_open("planning.db", &db);
+    int rc = sqlite3_open("planning.db", &m_db);
 
     if(rc) {
         std::cout << "can't open it" << std::endl;
     } else {
         std::cout << "opened database successfully" << std::endl;
+
     }
+
+}
+
+void sqliteEmployee::close() {
+    sqlite3_close(m_db);
+}
+
+
+void sqliteEmployee::createDefaultDataBase() {
+    std::string sql = "CREATE TABLE COMPANY("  \
+                       "ID INT PRIMARY KEY     NOT NULL," \
+                       "NAME           TEXT    NOT NULL," \
+                       "AGE            INT     NOT NULL," \
+                       "ADDRESS        CHAR(50)," \
+                       "SALARY         REAL );";
+    rc = sqlite3_exec(m_db, sql, )
 }
